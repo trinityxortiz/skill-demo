15a8c8c7352b5281c7fe1f21100bad9f4634f9b9
trinityxortiz <t3ortiz@ucsd.edu>
2022-05-01 23:42:41 -0700

    fail

--- Tester.java
+++ Tester.java
@@ -1,5 +1,9 @@
 import static org.junit.Assert.*;
 import org.junit.*;
+
 public class Tester {
-  
+    @Test
+    public void testMultiply(){
+        assertEquals(2,Demo.multiply(1, 2));
+    }
 }
